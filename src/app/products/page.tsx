7e986d46ff5272809a463ef1fb1f1ff201750514
danishyameen <danishyameennew@gmail.com>
2025-01-17 04:20:10 +0500

    update project

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+export default function productsPage(){
+    return(
+        <div className="productsPage header_container">
+            <div>
+                <div className="mb-5">
+                    <h2 className="text-[24px] font-Helvetica font-medium">New (500)</h2>
+                </div>
+                <div>
+                <h2 className="text-[16px] font-Helvetica font-normal ">Hide Filters</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
